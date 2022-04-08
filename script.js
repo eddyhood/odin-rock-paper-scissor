bd9c5ba0d06b675a237de6573f84883ae00c44be
Eddy Hood <hood@MacBook-Pro-2.local>
2022-04-07 21:41:24 -0600

    Reduce and simplify code
    I broke up the gamePlay code so that that function would only accomplish
    the logic for the game.  The variables for the player choice and the computer
    choice were separated from the function and then set up as parameters to
    be passed in.

--- script.js
+++ script.js
@@ -1,51 +1,40 @@
+
+//Generate a random move for the computer
 function computerPlay() {
     let moveOptions = ['rock', 'paper', 'scissors']
     let computerMove = moveOptions[Math.floor(Math.random()*moveOptions.length)];
     return computerMove;
 }
 
-function gamePlay() {
-    let userMove = prompt('Type: rock, paper, or scissors');
-    let computerMove = computerPlay();
-    if(userMove === 'rock' && computerMove ==='scissors') {
-        console.log('Computer chose: scissors')
-        console.log('You chose: rock')
-        console.log('You win!');
-    }else if(userMove === 'rock' && computerMove ==='paper') {
-        console.log('Computer chose: paper')
-        console.log('You chose: rock')
-        console.log('You loose!');
-    }else if(userMove === 'rock' && computerMove === 'rock') {
-        console.log('Computer chose: rock')
-        console.log('You chose: rock')
-        console.log('It\'s a tie!');
-    }else if(userMove === 'paper' && computerMove === 'rock') {
-        console.log('Computer chose: rock')
-        console.log('You chose: paper')
-        console.log('You win!');
-    }else if(userMove === 'paper' && computerMove === 'scissors') {
-        console.log('Computer chose: scissors')
-        console.log('You chose: paper')
-        console.log('You loose!');
-    }else if(userMove === 'paper' && computerMove === 'paper') {
-        console.log('Computer chose: paper')
-        console.log('You chose: paper')
-        console.log('It\'s a tie!');
-    }else if(userMove === 'scissors' && computerMove ==='paper') {
-        console.log('Computer chose: paper')
-        console.log('You chose: scissors')
-        console.log('You win!');
-    }else if(userMove === 'scissors' && computerMove ==='rock') {
-        console.log('Computer chose: rock')
-        console.log('You chose: scissors')
-        console.log('You lose!');
-    }else if(userMove === 'scissors' && computerMove === 'scissors'){
-        console.log('Computer chose: scissors')
-        console.log('You chose: scissors')
-        console.log('It\'s a tie!');
+//Declare variables for both the computer and the user
+let player = prompt('Type: rock, paper, or scissors').toLowerCase();
+let computer = computerPlay();
+
+
+//Run the game logic after passing in computer and user selections
+function game(playerSelection, computerSelection) {
+    if(player === 'rock' && computer ==='scissors') {
+        console.log('You win! Rock beats scissors.');
+    }else if(player === 'rock' && computer ==='paper') {
+        console.log('You lose! Paper beats rock.');
+    }else if(player === 'rock' && computer === 'rock') {
+        console.log('It\'s a tie. Rock = rock.');
+    }else if(player === 'paper' && computer === 'rock') {
+        console.log('You win! Paper beats rock.');
+    }else if(player === 'paper' && computer === 'scissors') {
+        console.log('You lose! Scissors beats paper.');
+    }else if(player === 'paper' && computer === 'paper') {
+        console.log('It\s a tie. Paper = paper.');
+    }else if(player === 'scissors' && computer ==='paper') {
+        console.log('You win! Scissors beats paper.');
+    }else if(player === 'scissors' && computer ==='rock') {
+        console.log('You lose! Rock beats scissors.');
+    }else if(player === 'scissors' && computer === 'scissors'){
+        console.log('It\'s a tie! Scissors = scissors.');
     }else {
-        console.log("Hmmm. Something didn't work. Try again")
+        console.log('Hmmm, something went wrong. Try again.');
     }
 }
 
-gamePlay()
+game(player, computer)
+
